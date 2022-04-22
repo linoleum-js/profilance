5314d539f462a55ddc8c5b850fdccd74176e9f90
martinschulz <martinschulzofficial@gmail.com>
2022-04-22 15:22:33 +0500

    Project structure

---
+++ src/models/IPost.js
@@ -0,0 +1,9 @@
+/**
+ * @typedef {object} IPost
+ *
+ * @property {string} title
+ * @property {string} text
+ * @property {number} createdAt
+ * @property {boolean} isApproved
+ */
+
---
+++ src/models/IUser.js
@@ -0,0 +1,6 @@
+/**
+ * @typedef {object} IUser
+ *
+ * @property {string} username
+ * @property {role}
+ */
